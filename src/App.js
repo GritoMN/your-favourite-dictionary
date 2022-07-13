6cdd0b6c21f945dcec79326e82d378acb3fbcd99
Gr1t0 <nikolova.grigoriya@gmail.com>
2022-07-13 20:27:37 +0200

    added  link to github

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"> Your Favourite Dictionary App</header>
-      <main>
-        <Dictionary defaultKeyword="garden" />
-      </main>
+    <div className="linkGit">
+      {" "}
+      This website was coded by Grigoriya Nikolova, and is
+      <a
+        href="https://github.com/GritoMN/your-favourite-dictionary"
+        target="_blank"
+      >
+        {" "}
+        open-sourced
+      </a>
+      <div className="App">
+        <header className="App-header"> Your Favourite Dictionary App</header>
+        <main>
+          <Dictionary defaultKeyword="garden" />
+        </main>
+      </div>
     </div>
   );
 }
